e9547b3cad0358339e7e216e79b3c6f74853bba1
Francis Kobina Kum <kumfrancis.fk@gmail.com>
2026-01-09 01:33:39 +0000

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   )
 }
