15b6a3b8a638f90cb7e5eae120a78c3896e4665f
Francis Kobina Kum <kumfrancis.fk@gmail.com>
2026-02-21 19:40:38 +0000

    Fix TodoList.test.js to match ALX checker requirements

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -12,31 +12,24 @@ describe("TodoList Component", () => {
 
   test("adds a new todo", () => {
     render(<TodoList />);
-
     const input = screen.getByPlaceholderText("Add a todo");
     const button = screen.getByText("Add");
-
     fireEvent.change(input, { target: { value: "New Task" } });
     fireEvent.click(button);
-
     expect(screen.getByText("New Task")).toBeInTheDocument();
   });
 
   test("toggles a todo", () => {
     render(<TodoList />);
-
     const todo = screen.getByText("Learn React");
     fireEvent.click(todo);
-
     expect(todo).toHaveStyle("text-decoration: line-through");
   });
 
   test("deletes a todo", () => {
     render(<TodoList />);
-
     const deleteButtons = screen.getAllByText("Delete");
     fireEvent.click(deleteButtons[0]);
-
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
   });
 
